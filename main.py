2e1837a057030f9596cb423abed29adf3d94dd12
Mikel Aldalur <mikelalda13@gmail.com>
2023-04-25 21:08:22 +0200

    c

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def gstreamer_pipeline(
 
 
 # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-print(gstreamer_pipeline(flip_method=0))
+# print(gstreamer_pipeline(flip_method=0))
 def inference():
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     if cap.isOpened():
