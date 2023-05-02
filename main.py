34ba71fe81fec6953a5f3a8a15a36a4da34028dc
Mikel Aldalur <mikelalda13@gmail.com>
2023-05-02 22:11:41 +0200

    tracking in thread

--- main.py
+++ main.py
@@ -53,9 +53,8 @@ def laguntza(message):
 @bot.message_handler(commands=['piztu', 'encender', 'turn_on'])
 def piztu(message):
     gv.DETECTION_RUNNING = True
-    # thread1 = threading.Thread(target=tracking, args=(message,))
-    # thread1.start()
-    tracking(message)
+    thread1 = threading.Thread(target=tracking, args=(message,))
+    thread1.start()
     bot.send_message(message.chat.id, "Piztu da - Se ha encendido - Turned on")
 
 @bot.message_handler(commands=['itzali', 'apagar', 'turn_off'])
